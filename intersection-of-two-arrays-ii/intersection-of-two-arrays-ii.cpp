2e8b02ff5c1abcf1d8930f6a6821a26caf282170
Nomaan-15 <124508608+Nomaan-15@users.noreply.github.com>
2024-08-13 15:05:46 +0530

    Time: 3 ms, Memory: 12.8 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -3,23 +3,28 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         
         
-        unordered_map<int,int>mp;
+        
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
         vector<int> ans;
         
-        for(int i=0;i<nums1.size();i++){
-            mp[nums1[i]]++;
-        }
-        
-        
-        for(int i=0;i<nums2.size();i++){
-            if(mp[nums2[i]] > 0){
-                ans.push_back(nums2[i]);
-                mp[nums2[i]]--;
+         int i=0;
+          int j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            
+            if(nums1[i] == nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                i++;
             }
         }
         
-        
         return ans;
-        
     }
 };
